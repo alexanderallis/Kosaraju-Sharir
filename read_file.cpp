216bdec4b5d8eb8bf8a563acd8d6422f2e452899
Alexander Allis <alexandreallis@gmail.com>
2022-10-19 14:11:10 -0400

    Deleted readFile() and added documentation to getPairsFromStdIn().

--- read_file.cpp
+++ read_file.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <utility>
 #include <vector>
@@ -11,6 +10,16 @@
 #include "read_file.h"
 
 using std::ifstream;
+
+/*
+ * Gets the list of pairs from the standard input according to the format specified in main.cpp
+ * Args:
+ *  pairs: modifies list of pairs inplace
+ *  numberOfVertices: modifies inplace
+ *  numberOfEdges: modifies inplace
+ * Returns:
+ *  0 if executed
+ */
 
 int getPairsFromStdIn(std::vector<std::pair<int, int>> & pairs, int & numberOfVertices, int & numberOfEdges) {
 
@@ -46,62 +55,5 @@ int getPairsFromStdIn(std::vector<std::pair<int, int>> & pairs, int & numberOfVe
         getline(std::cin, line);
     }
 
-    return 0;
-}
-
-int readFile(const std::string & fileName, std::vector<std::pair<int, int>> & treePairs, int & numberOfVertices, int & numberOfEdges) {
-
-    // Check if file exists
-    ifstream inFile(fileName.c_str());  // Check to make sure file exists
-    if (!inFile) {
-        std::cerr << "File not found." << std::endl;
-        return -1;
-        // Or throw error
-    }
-
-// Open file
-    ifstream fileStream(fileName);
-    if (!fileStream.is_open()) {
-        std::cout << "Can't read file." << std::endl;
-        return -1;
-        // Or throw error
-    }
-
-    std::stringstream sStream;  // stream to parse each line
-    std::string line;  // each line (string)
-    std::vector<int> row;  // each line (int)
-    std::string vectorA;  // each vertex
-    std::string vectorB;
-
-    getline(fileStream, line);  // Consume first line
-    numberOfVertices = stoi(line);
-
-    getline(fileStream, line);  // Consume second line
-    numberOfEdges = stoi(line);
-
-    while(!fileStream.eof()) {  // Loop through lines
-
-        getline(fileStream, line);
-
-        if (!fileStream.fail()) {
-            sStream << line;
-            while(!sStream.eof()) {  // Loop through numbers
-                if(!sStream.fail()) {
-                    sStream >> vectorA;
-                    sStream >> vectorB;
-                    treePairs.emplace_back(std::stoi(vectorA), std::stoi(vectorB));
-                }
-            }
-
-            // Clean up
-            sStream.str(std::string());  // Clear string
-            sStream.clear();  // Clear the state flags for eof()
-            for (int &k: row) {    // Clear contents of the vector
-                row.clear();
-            }
-        }
-    }
-
-    fileStream.close();
     return 0;
 }
